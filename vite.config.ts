63da45c87e28785554593bba12490d4a857fb3f6
AlejandroCedeno06 <cedenoalejandro0612@gmail.com>
2026-04-29 00:17:20 -0600

    ARREGLAR BUIGS

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Empresa-de-Desarrollo-Web/',
   plugins: [
     // React y Tailwind son necesarios para Make, aunque Tailwind no se use activamente – no los elimines
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      // Alias @ apunta al directorio src
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 
   // Tipos de archivo que soportan importación raw. Nunca agregues .css, .tsx o .ts aquí.
   assetsInclude: ['**/*.svg', '**/*.csv'],
